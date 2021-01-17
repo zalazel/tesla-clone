ed262390f8d1182b3754df079d23e7499303d692
Mohamed Mustafa <zalazel@Mohameds-MacBook-Pro.local>
2021-01-17 19:12:01 +0200

    Header + Assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-
-      <h1>Clever Programmer</h1>
+      <Header/>
       
     </div>
   );
